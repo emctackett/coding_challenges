edcc8c33a1a2fb88df9dd0dd5d99c7e06750181d
Elizabeth Tackett <emctackett@gmail.com>
2016-11-27 10:18:05 -0600

    completed series code challenge

--- series.rb
+++ series.rb
@@ -1,20 +1,14 @@
-# Write a program that will take a string of digits and give you all the
-# possible consecutive number series of length n in that string.
-
 class Series
-  def initialize(string)
-    @numbers = string.chars.map(&:to_i)
+  attr_reader :digits
+
+  def initialize(digits)
+    @digits = digits.chars.map(&:to_i)
   end
 
-  def slices(count)
-    raise ArgumentError, 'count is too big.' if count > @numbers.size
-
-    @numbers.each_cons(count).to_a
+  def slices(n)
+    raise ArgumentError, 'n greater than string size' if n > digits.size
+    result = []
+    digits.each_cons(n) { |chunk| result << chunk }
+    result
   end
 end
-
-series = Series.new("01234")
-p series.slices(3)
-
-second_series = Series.new("01234")
-p second_series.slices(2)
