e9ad52081a60dbf54fa0f579d1170a59307cb625
Elizabeth Tackett <emctackett@gmail.com>
2016-11-27 13:56:11 -0600

    removed excess code line

--- odd_words.rb
+++ odd_words.rb
@@ -12,16 +12,3 @@ def odd_words(input_text)
     reversed_odds.join(' ')
   end
 end
-
-# test cases
-p odd_words('whats the matter with kansas.')
-p odd_words('hello') #=> 'hello'
-p odd_words('hello word.') #=> 'hello drow.'
-p odd_words('hello word .') #=> 'hello drow.'
-p odd_words('hello  word .') #=> 'hello drow.'
-p odd_words('hello word  .') #=> 'hello drow.'
-p odd_words('hello word world .') #=> 'hello drow world.'
-
-p odd_words('') #=> ''
-p odd_words('.') #=> '.'
-p odd_words(' hello . ')
